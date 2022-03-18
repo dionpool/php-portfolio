73697d36c3d0b0a39fc5e5f725828fccc062907f
dionpool <elirionn@gmail.com>
2022-03-18 15:10:18 +0100

    Added basic login form

--- index.php
+++ index.php
@@ -11,8 +11,34 @@
 
     <!-- Stylesheet -->
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/uikit.min.css">
 </head>
 <body>
-
+   <div class="uk-section uk-container">
+      <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1">
+         <form class="uk-form-stacked js-login">
+            <div class="uk-margin">
+               <label class="uk-form-label" for="form-stacked-text">Email</label>
+               <div class="uk-form-controls">
+                  <input class="uk-input" id="form-stacked-text" type="email" placeholder="Enter email address..." required>
+               </div>
+            </div>
+   
+            <div class="uk-margin">
+               <label class="uk-form-label" for="form-stacked-text">Password</label>
+               <div class="uk-form-controls">
+                  <input class="uk-input" id="form-stacked-text" type="password" placeholder="Enter password..." required>
+               </div>
+            </div>
+      
+            <div class="uk-margin">
+               <button class="uk-button uk-button-default" type="submit">Login</button>
+         </form>
+      </div>
+   </div>
+   
+   <!-- JavaScript -->
+   <script src="js/uikit.min.js"></script>
+   <script src="js/uikit-icons.min.js"></script>
 </body>
 </html>
