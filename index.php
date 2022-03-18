bf946164dd7be1228b8928568f57fcc140ebd4df
dionpool <elirionn@gmail.com>
2022-03-18 14:49:40 +0100

    Added index HTML markup and main stylesheet

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php echo "Hello World"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Title -->
+    <title>Document</title>
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+</body>
+</html>
