220e6ffae7a8b9b00aaee523166d20b73e0ad304
dionpool <elirionn@gmail.com>
2022-03-25 09:47:46 +0100

    Adds includes with head and footer includes.

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <!-- Meta -->
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<?php include 'includes/head.php'; ?>
 
-   <!-- Title -->
-   <title>Document</title>
-
-   <!-- Stylesheet -->
-   <link rel="stylesheet" href="css/style.css">
-   
-   <!-- Bootstrap CSS -->
-   <link rel="stylesheet" href="css/bootstrap.min.css">
-</head>
 <body>
    
    
@@ -24,4 +9,5 @@
    <!-- Bootstrap JS -->
    <script src="js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
+
+<?php include 'includes/footer.php'; ?>
