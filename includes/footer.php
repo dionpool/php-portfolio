36652112279efce5ee00349becb9131f54d96565
dionpool <elirionn@gmail.com>
2022-03-25 09:51:12 +0100

    Adds navigation file and puts JavaScript in footer.

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,7 @@
+   <!-- jQuery -->
+   <script src="js/jquery-3.6.0.min.js"></script>
+   
+   <!-- Bootstrap JS -->
+   <script src="js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php include 'includes/head.php'; ?>
 
-<body>
-   
-   
-   <!-- jQuery -->
-   <script src="js/jquery-3.6.0.min.js"></script>
-   
-   <!-- Bootstrap JS -->
-   <script src="js/bootstrap.bundle.min.js"></script>
-</body>
+<?php include 'includes/navigation.php'; ?>
 
 <?php include 'includes/footer.php'; ?>
