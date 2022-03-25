f21bb41451b691f5ba48ea16670f6af1963f851f
dionpool <elirionn@gmail.com>
2022-03-25 09:40:53 +0100

    Adds Bootstrap stylesheets and JavaScript.

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Meta -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <!-- Meta -->
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <!-- Title -->
-    <title>Document</title>
+   <!-- Title -->
+   <title>Document</title>
 
-    <!-- Stylesheet -->
-    <link rel="stylesheet" href="css/style.css">
+   <!-- Stylesheet -->
+   <link rel="stylesheet" href="css/style.css">
+   
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
    
@@ -18,6 +21,7 @@
    <!-- jQuery -->
    <script src="js/jquery-3.6.0.min.js"></script>
    
-   
+   <!-- Bootstrap JS -->
+   <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
